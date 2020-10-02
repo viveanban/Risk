1a7b5dcfacdfc938906e3ae3b179eb12184db9f5
imabhijit <abhijit.gupta1122@gmail.com>
2020-10-01 20:24:30 -0400

    added test for copy constructor

--- Map/MapDriver.cpp
+++ Map/MapDriver.cpp
@@ -10,5 +10,12 @@ void MapDriver::run(Graph *map){
     bool validateOutcome = (*map).validate();
 
     cout << "validate outcome: " << boolalpha << validateOutcome << endl;
+
+//    Test for copy constructor
+//    Territory t1 = *(map->getTerritoryList().at(0));
+//    Territory t2 = Territory(t1);
+//    t1.getAdjList().at(0) = {};
+//    cout << t1.getAdjList().at(0) << " t2: " << t2.getAdjList().at(0);
+
 }
 
