1800201942174bcbf4763fd5095040ba54181428
Tarek Ait Hamouda <tarekaithamouda@gmail.com>
2020-10-02 22:50:31 -0400

    added back main of MapDriver.cpp

--- Map/MapDriver.cpp
+++ Map/MapDriver.cpp
@@ -1,25 +1,18 @@
-//#include "./Map.h"
-//#include "./MapDriver.h"
-//#include "./../MapLoader/MapLoader.h"
-//using namespace std;
-//
-//int main(Graph *map){
-//    MapLoaderDriver mapLoaderDriver;
-//    Graph *map;
-//    map = mapLoaderDriver.run();
-//
-//
-//    (*(*map).getContinentList().at(0)).setTerritories({});
-//
-//    bool validateOutcome = (*map).validate();
-//
-//    cout << "validate outcome: " << boolalpha << validateOutcome << endl;
-//
-////    Test for copy constructor
-////    Territory t1 = *(map->getTerritoryList().at(0));
-////    Territory t2 = Territory(t1);
-////    t1.getAdjList().at(0) = {};
-////    cout << t1.getAdjList().at(0) << " t2: " << t2.getAdjList().at(0);
-//
-//}
-//
+#include "./Map.h"
+#include "./../MapLoader/MapLoader.h"
+using namespace std;
+
+int main(){
+    MapLoader mapLoader;
+    Graph *map;
+    map = mapLoader.loadMap();
+
+    //in order to break the continent-territory mapping
+    //(*(*map).getContinentList().at(0)).setTerritories({});
+
+    bool validateOutcome = (*map).validate();
+
+    cout << "validate outcome: " << boolalpha << validateOutcome << endl;
+
+}
+
