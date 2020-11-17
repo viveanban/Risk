ed033ae208e5f7c4984ceab57e7651b4e0613614
Viveka <viveka98@hotmail.ca>
2020-11-16 19:30:09 -0500

    Issuin glogic changed a bit

--- Player/Player.cpp
+++ Player/Player.cpp
@@ -10,7 +10,7 @@
 Player *Player::neutralPlayer = new Player("Neutral Player");
 
 Player::Player(string playerName) : playerName(playerName), handOfCards(new Hand()), orders(new OrdersList()),
-                                    territories() {}
+                                    numberOfArmies(0), territories() {}
 
 Player::~Player() {
     delete handOfCards;
@@ -141,7 +141,7 @@ bool Player::issueOrder() {
         bool continueIssuingOrders = rand() % 2;
 
         if (continueIssuingOrders) {
-            bool advance = rand() % 2;
+            bool advance = handOfCards->getCards().empty() || rand() % 2;
             if (advance) {
                 (new AdvanceOrder(this))->issue();
             } else {
