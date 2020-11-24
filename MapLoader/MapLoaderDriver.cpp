7bcc5e427f9693ef9e16f7da46be5867c0cab334
Tarek Ait Hamouda <tarekaithamouda@gmail.com>
2020-11-24 17:57:36 -0500

    updated MapLoaderDriver

--- MapLoader/MapLoaderDriver.cpp
+++ MapLoader/MapLoaderDriver.cpp
@@ -1,40 +1,42 @@
-//TODO: Adapt old MapLoaderDriver to demonstrate MapLoader and ConquestFileReader
-
 //TODO: Test ConquestFileReader with Asia.map (validation of connected continents fail; check if it's true)
-//#include <fstream>
-//#include "MapLoader.h"
-//
-//const string MAP_DIRECTORY = "../maps/";
-//
-//string getUserInput() {
-//    string userInput = "";
-//    ifstream infile;
-//
-//    cout << "Enter your desired map:  " << std::endl;
-//    cin >> userInput;
-//
-//    infile.open(MAP_DIRECTORY + userInput);
-//    while (!infile) {
-//        cout << "This map does not exist. Please enter another map: \n";
-//        cin >> userInput;
-//        infile.open(MAP_DIRECTORY + userInput);
-//    }
-//
-//    return userInput;
-//}
-//
-//
-//int main() {
-//    cout << "Getting input from player.. \n";
-//    string userInput = getUserInput();
-//
-//    cout << "Map is being loaded.. \n";
-//    Graph *map;
-//    map = MapLoader::loadMap(userInput);
-//    cout << *map << endl;
-//
-//    delete map;
-//    map = nullptr;
-//}
+#include <fstream>
+#include "MapLoader.h"
+
+int main() {
+
+    string conquestMapsToValidate[] = {"Africa.map", "Asia.map", "UnconnectedAfrica.map", "InvalidAfrica.map"};
+    string regularMapsToValidate[] = {"Europe.map", "USInvalid"};
+    Map *map;
+    ConquestFileReader conquestFileReader;
+    MapLoader *conquestMapLoader = new ConquestFileReaderAdapter(conquestFileReader);
+    MapLoader *mapLoader = new MapLoader();
+
+    for (string mapName: conquestMapsToValidate) {
+        cout << "==========================================================" << endl;
+        cout << "Loading " << mapName << " from maps/conquest_maps/" << endl;
+        map = conquestMapLoader->loadMap(mapName);
+        if (map != nullptr) {
+            cout << *map << endl;
+            map->validate();
+        }
+        cout << "==========================================================" << endl;
+
+    }
+    for (string mapName: regularMapsToValidate) {
+        cout << "==========================================================" << endl;
+        cout << "Loading " << mapName << " from maps/" << endl;
+        map = mapLoader->loadMap(mapName);
+        if (map != nullptr) {
+            cout << *map << endl;
+            map->validate();
+        }
+        cout << "==========================================================" << endl;
+
+    }
+
+    delete map;
+    delete conquestMapLoader;
+    delete mapLoader;
+}
 
 
