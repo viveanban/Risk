836d73c47d071723de81519415a67e96a6968b30
viveka <viveka98@hotmail.ca>
2020-11-23 22:45:39 -0500

    Added TODOs for what's left

--- GameEngine/GameEngine.cpp
+++ GameEngine/GameEngine.cpp
@@ -27,7 +27,7 @@ void GameEngine::initializeGame() {
 }
 
 void GameEngine::selectMap() {
-    //TODO: Change the map directory according to the type of maps the user wants
+    //TODO: Change the map directory according to the type of map
     const string MAP_DIRECTORY = "../maps/conquest_maps/";
     int chosenMap;
     ifstream inputFile;
